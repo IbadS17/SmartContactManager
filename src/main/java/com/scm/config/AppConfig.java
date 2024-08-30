9f9e7a87e977af3c25c4b94ff300957522e5ba30
unknown <ibads66585@gmail.com>
2024-08-31 00:45:46 +0530

    Your commit message here

--- src/main/java/com/scm/config/AppConfig.java
+++ src/main/java/com/scm/config/AppConfig.java
@@ -20,13 +20,11 @@ public class AppConfig {
     private String apiSecret;
 
     @Bean
-    public Cloudinary cloudinary(){
+    public Cloudinary cloudinary() {
         return new Cloudinary(
-            ObjectUtils.asMap(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret
-            )   
-        );
+                ObjectUtils.asMap(
+                        "cloud_name", cloudName,
+                        "api_key", apiKey,
+                        "api_secret", apiSecret));
     }
 }
